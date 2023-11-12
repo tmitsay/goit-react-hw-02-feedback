d2bbc375c50e0b414d42e6af9df5b2686c7b0951
tmitsay <132346077+tmitsay@users.noreply.github.com>
2023-11-12 17:15:36 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Statistics } from './Statistics/statistics';
+import { Sections } from './Section/section';
+import { Notification } from './Notification/notification';
+import { Buttons } from './FeedbackOptions/feedbackOptions';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = state => {
+    this.setState(prevState => ({ [state]: prevState[state] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + bad + neutral;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return Math.round((good / total) * 100) || 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
+    const totalFeedback = this.countTotalFeedback();
+    const totalPercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div>
+        <Sections title="Please leave feedback">
+          <Buttons onLeaveFeedback={this.onLeaveFeedback} options={options} />
+        </Sections>
+        <Sections title="Statistics">
+          {totalFeedback !== 0 ? (
+            <Statistics
+              good={good}
+              bad={bad}
+              neutral={neutral}
+              total={totalFeedback}
+              positivePercentage={totalPercentage}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Sections>
+      </div>
+    );
+  }
+}
